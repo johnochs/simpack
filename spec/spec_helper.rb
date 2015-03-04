c06611fa93693c78820e157c159f23c24eb7b15f
John Ochs <johnochs@me.com>
2015-03-04 15:47:30 -0800

    Adds code climate repo token to ENV on testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require "codeclimate-test-reporter"
+ENV['CODECLIMATE_REPO_TOKEN'] = '434d4cfa388ad2146c71bca82bf4cb9693336f208412c7378c2c099de8f81326'
 CodeClimate::TestReporter.start
 
 require 'simpack'
