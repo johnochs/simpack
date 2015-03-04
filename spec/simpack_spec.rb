d45057988b0009749843f4946c4e49a20621120c
John Ochs <johnochs@me.com>
2015-03-04 14:18:43 -0800

    Adds tests for Simpack::LCG #initialize and #uniform

--- spec/simpack_spec.rb
+++ spec/simpack_spec.rb
@@ -44,6 +44,73 @@ describe Simpack do
 
     end
 
+    describe '#sample' do
+
+      subject(:lcg) { Simpack::LCG.new }
+
+      it 'can be called with out an argument to return a single number' do
+        expect(lcg.uniform).to be_kind_of(Float)
+      end
+
+      it 'can be called with an integer argument to return an array of numbers' do
+        result = lcg.uniform(5)
+        expect(result).to be_kind_of(Array)
+        expect(result).to all( be_a(Float) )
+      end
+
+      it 'when called with an integer argument, returns that number of items' do
+        rand_num = (100 * rand()).floor
+        expect(lcg.uniform(rand_num).size).to eq(rand_num)
+      end
+
+    end
+
+    context 'correctness of output' do
+
+      describe 'general conditions' do
+
+        it 'only outputs numbers between 0 & 1' do
+          results = Simpack::LCG.new.uniform(10000)
+          expect(results).to all( be > 0.0 )
+          expect(results).to all( be < 1.0 )
+        end
+
+      end
+
+      describe 'tests' do
+
+        it 'passes chi-squared test for alpha = 0.05' do
+          test_statistics = []
+          chi_squared_quantile = 16.92
+
+          100.times do
+            sample = Simpack::LCG.new.uniform(10000)
+
+            fis = []
+            k = 10
+
+            # Calculates number of random numbers falling into subintervals
+            # [(i-1)/k, i/k]
+            (1..k).each do |i|
+              fis << sample.select { |el| el.between?((i - 1)/k.to_f, i/k.to_f) }.size
+            end
+
+            sum = 0
+
+            (1..k).each do |i|
+              sum += (fis[i - 1].to_f - sample.size.to_f / k) ** 2
+            end
+
+            chi_squared_test_statistic = (k.to_f / sample.size) * sum
+            test_statistics << chi_squared_test_statistic
+          end
+
+          expect(test_statistics.count { |i| i < chi_squared_quantile }).to be >= 95
+        end
+      end
+
+    end
+
   end
 
 end
